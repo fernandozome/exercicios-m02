cc3e4b2040f996e087e632de2b96c04fb4544b6a
Fernando Zome <fernandozome@gmail.com>
2023-08-26 22:29:18 -0300

    Organizando...

--- Aula 03/Exericio resolvido 1/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-const { fltr_carros, listar_carros } = require("./Controladores/listagem carros")
-
-
-app.get("/carros", listar_carros)
-app.get("/carros/:id", fltr_carros)
-
-app.listen(3000)
